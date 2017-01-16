cc77f1a3451e7abd69307f554c9b0133ee5dc5f7
David THIBAU <david.thibau@gmail.com>
2017-01-16 11:56:40 +0100

    Passage en majuscule : niveau dune formation

---
+++ migration/3.1.5/niveau.sql
@@ -0,0 +1,3 @@
+update formation set for_niveau='Débutant' where for_niveau='débutant';
+update formation set for_niveau='Intermédiaire' where for_niveau='intermédiaire';
+update formation set for_niveau='Expert' where for_niveau='expert';

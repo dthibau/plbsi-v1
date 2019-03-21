5e17e90a997e44f30dd8fcaf01b5a1a6a2bc82c7
David THIBAU <david.thibau@gmail.com>
2019-03-21 13:27:21 +0100

    Deployement script

---
+++ deployProd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "Pleaaase ! Indiquer un tag "
+    exit 1
+fi
+
+
+
+
+# Bakup n-1
+cp jboss-eap-6.4/standalone/deployments/plbsi.war plbsi.war.n-1
+
+# Build with prod profile
+ant clean deploy -Djboss.home=/home/jboss/jboss-eap-6.4 -Dserver=http://plbsi-prod.plb.fr:8081 -Dprofile=prod -Djasper_home=/home/jboss/jasperreports-5.5.0
+
+git tag $1
+git push origin $1
+
+

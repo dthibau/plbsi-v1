113f3c73cebffff9728d8da2c61a15542afe59ed
David THIBAU <david.thibau@gmail.com>
2019-12-13 16:58:01 +0100

    Ajout EDOF dans typecontact

--- migration/4.3.2/update_niveau.sql
+++ migration/4.3.2/update_niveau.sql
@@ -1,2 +1,5 @@
 update formation set for_niveau='Fondamental' where for_niveau='Débutant';
 update formation set for_niveau='Avancé' where for_niveau='Expert';
+
+delete from typecontact where id_typecontact=18;
+insert into typecontact (id_typecontact,libelle,DTYPE) values (18,'EDOF','SI');

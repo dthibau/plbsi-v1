004b04b95a1d42817ba304da8e2a23b69a017d53
David THIBAU <david.thibau@gmail.com>
2020-03-25 11:39:40 +0100

    Mise à jour modalites de suivi

---
+++ migration/4.3.7/update_modalites.sql
@@ -0,0 +1,1 @@
+update formation set modalites_suivi=CONCAT(modalites_suivi,'Si les conditions le permettent, nous pouvons inscrire jusqu''à 24h avant le début de la session. Les convocations, avec lieux, horaires définitifs et plan d''accès, sont envoyées deux semaines avant le début de la formation.');

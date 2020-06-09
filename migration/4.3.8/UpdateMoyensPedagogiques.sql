921e9e9a1f6b153fd5511448ba60d8af123aba4b
David THIBAU <david.thibau@gmail.com>
2020-06-09 08:19:50 +0200

    Mise à jour en masse Moyens pedagogiques pour Qualiopi

---
+++ migration/4.3.8/UpdateMoyensPedagogiques.sql
@@ -0,0 +1,6 @@
+update formation set moyens_pedagogiques='Les formations PLB sont conçues et animées par des experts en activité.<br/>
+Nos salles sont équipées de vidéoprojecteur et écran/tableau interactif, accès Internet très haut débit et espace documentaire partagé. Chaque participant dispose d''un poste de travail et d’un support de cours.<br/>
+La plupart des formations peuvent être suivies à distance et en direct (voir les <a href="/plbConnect.php">Modalités pédagogiques, techniques et de suivi</a> spécifiques aux formations à distance synchrone).<br/>
+<br/>
+Les cours et examens de certification officiels suivent les conditions de l''éditeur, du constructeur ou du certificateur. Ils sont susceptibles d''évoluer à tout moment.<br/>
+Notre bâtiment est classé ERP 5. En cas de handicap, contactez-nous afin de mettre en place l''équipement et l''accompagnement adaptés.';

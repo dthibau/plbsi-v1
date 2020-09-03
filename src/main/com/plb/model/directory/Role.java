9b082ab7a0b94bd79e58d103d674efffe2fdd5a7
David THIBAU <david.thibau@gmail.com>
2020-09-03 12:34:54 +0200

    Ajout de ACCOUNT_MANAGER

--- src/main/com/plb/model/directory/Role.java
+++ src/main/com/plb/model/directory/Role.java
@@ -1,7 +1,7 @@
 package com.plb.model.directory;
 
 public enum Role {
-	MANAGER("manager"),DISPATCHER("dispatcher"),COMMERCIAL("commercial"),INTERVENANTS_MANAGER("intervenantsManager"),TB_MANAGER("tableauBordManager");
+	MANAGER("manager"),DISPATCHER("dispatcher"),COMMERCIAL("commercial"),INTERVENANTS_MANAGER("intervenantsManager"),TB_MANAGER("tableauBordManager"),ACCOUNT_MANAGER("AC_MANAGER");
 	
 	private final String libelle; 
     Role(String libelle) {

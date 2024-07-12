1aede419f5290ea9408a439ed21acc0c2a69682a
David THIBAU <david.thibau@gmail.com>
2024-07-12 14:09:11 +0200

    Ajout du rôle OFRRE_MANAGER

--- src/main/com/plb/model/directory/Role.java
+++ src/main/com/plb/model/directory/Role.java
@@ -1,7 +1,7 @@
 package com.plb.model.directory;
 
 public enum Role {
-	MANAGER("manager"),DISPATCHER("dispatcher"),COMMERCIAL("commercial"),INTERVENANTS_MANAGER("intervenantsManager"),TB_MANAGER("tableauBordManager"),AC_MANAGER("AccountsManager"),FA_MANAGER("FactureManager");
+	MANAGER("manager"),DISPATCHER("dispatcher"),COMMERCIAL("commercial"),INTERVENANTS_MANAGER("intervenantsManager"),TB_MANAGER("tableauBordManager"),AC_MANAGER("AccountsManager"),FA_MANAGER("FactureManager"),OFFRE_MANAGER("OffreManager");
 	
 	private final String libelle; 
     Role(String libelle) {

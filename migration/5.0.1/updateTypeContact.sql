fb0c490a55eafa904fa3af309e705e9169283688
David THIBAU <david.thibau@gmail.com>
2024-09-03 16:27:28 +0200

    Fix #351

--- migration/5.0.1/updateTypeContact.sql
+++ migration/5.0.1/updateTypeContact.sql
@@ -1,4 +1,4 @@
 update typecontact set libelle ='Téléphone Google' where libelle = 'Action Co : Opcalia';
 
-insert into typecontact (id_typecontact,libelle,DTYPE) values (21,'Chat', 'WEB');
+insert into typecontact (id_typecontact,libelle,DTYPE) values (21,'Chat', 'SI');
 
